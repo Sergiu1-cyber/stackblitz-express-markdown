0641279dff156ff036675d60bacabde6c4d7c5d1
Sergiu1-cyber <git@stackblitz.com>
2024-10-31 10:42:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-express-markdown
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Sergiu1-cyber/stackblitz-express-markdown)
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
